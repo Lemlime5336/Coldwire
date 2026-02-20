bef599fd6d33ca1c5bec34cc56effbcd6df95f30
Lemlime5336 <limola5336@gmail.com>
2026-02-20 14:48:13 +0800

    corrected imports for uploadHCmiddleware

--- server/routes/halalCertificateRoutes.js
+++ server/routes/halalCertificateRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { uploadCertificate, getCertificatesBySupplier, checkExpiredCertificates } = require('../controllers/halalCertificateController');
 const { protect, adminOnly } = require('../middleware/authMiddleware');
-const upload = require('../middleware/uploadMiddleware');
+const upload = require('../middleware/uploadHCMiddleware');
 
 router.post('/', protect, adminOnly, upload.single('certificate'), uploadCertificate);
 router.get('/expired', protect, adminOnly, checkExpiredCertificates);
